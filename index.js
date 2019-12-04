e620bb620be1e6dc6c174ab83130850007d7e8df
hinfan123 <hinfan123@yahoo.com>
2019-12-03 23:43:02 -0800

    updated map and links

--- index.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-  document.getElementById("chatroomA").href="https://discord.gg/aPC6gJ"
-}
-function myFunction(){
-  document.getElementById("chatroomB").href="https://discord.gg/6A6TTE"
-}
